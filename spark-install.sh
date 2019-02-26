6442d6c7690883a3408c5c47498ec4f2d8671fbd
turbowill4@gmail.com <turbowill4@gmail.com>
2019-02-25 22:03:39 -0500

    changed some stuff

--- spark-install.sh
+++ spark-install.sh
@@ -12,13 +12,12 @@ SHELL_PROFILE="$HOME/.bashrc"
 SPARK_INSTALL_LOCATION=$HOME
 
 # Specify the URL to download Spark from
-SPARK_URL=http://apache.mirrors.tds.net/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz
+SPARK_URL=http://apache.mirrors.tds.net/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
 
 # The Spark folder name should be the same as the name of the file being downloaded as specified in the SPARK_URL
-SPARK_FOLDER_NAME=spark-2.1.0-bin-hadoop2.7.tgz
+SPARK_FOLDER_NAME=spark-2.4.0-bin-hadoop2.7.tgz
 
-# Find the proper md5 hash from the Apache site
-SPARK_MD5=50e73f255f9bde50789ad5bd657c7a71
+
 
 # Print Disclaimer prior to running script
 echo "DISCLAIMER: This is an automated script for installing Spark but you should feel responsible for what you're doing!"
@@ -66,25 +65,6 @@ fi
 
 if [[ -d $HOME/scripts ]];
 then
-    # Create jupyspark.sh script in your scripts folder
-    read -r -p "Would you like to create the jupyspark.sh script for launching a local jupyter spark server? [y/N] " response
-    if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-        echo "#!/bin/bash
-        export PYSPARK_DRIVER_PYTHON=jupyter
-        export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook --NotebookApp.open_browser=True --NotebookApp.ip='localhost' --NotebookApp.port=8888\"
-
-        \${SPARK_HOME}/bin/pyspark \
-        --master local[4] \
-        --executor-memory 1G \
-        --driver-memory 1G \
-        --conf spark.sql.warehouse.dir=\"file:///tmp/spark-warehouse\" \
-        --packages com.databricks:spark-csv_2.11:1.5.0 \
-        --packages com.amazonaws:aws-java-sdk-pom:1.10.34 \
-        --packages org.apache.hadoop:hadoop-aws:2.7.3" > $HOME/scripts/jupyspark.sh
-
-        chmod +x $HOME/scripts/jupyspark.sh
-    fi
-
     # Create localsparksubmit.sh script in your scripts folder
     read -r -p "Would you like to create the localsparksubmit.sh script for submittiing local python scripts through spark-submit? [y/N] " response
     if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
@@ -132,9 +112,7 @@ then
     while [ $SUCCESSFUL_SPARK_INSTALL -eq 0 ]
     do
         curl $SPARK_URL > $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME
-        # Check MD5 Hash
-        if [[ $(openssl md5 $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME | sed -e "s/^.* //") == "$SPARK_MD5" ]]
-        then
+       
             # Unzip
             tar -xzf $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME -C $SPARK_INSTALL_LOCATION
             # Remove the compressed file
@@ -142,20 +120,7 @@ then
             # Install py4j
             pip install py4j
             SUCCESSFUL_SPARK_INSTALL=1
-        else
-            echo 'ERROR: Spark MD5 Hash does not match'
-            echo "$(openssl md5 $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME | sed -e "s/^.* //") != $SPARK_MD5"
-            if [ $SPARK_INSTALL_TRY -lt 3 ]
-            then
-                echo -e '\nTrying Spark Install Again...\n'
-                SPARK_INSTALL_TRY=$[$SPARK_INSTALL_TRY+1]
-                echo $SPARK_INSTALL_TRY
-            else
-                echo -e '\nSPARK INSTALL FAILED\n'
-                echo -e 'Check the MD5 Hash and run again'
-                exit 1
-            fi
-        fi
+       
     done
 elif [[ $(uname -s) = "Linux" ]]
 then
@@ -164,9 +129,7 @@ then
     while [ $SUCCESSFUL_SPARK_INSTALL -eq 0 ]
     do
         curl $SPARK_URL > $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME
-        # Check MD5 Hash
-        if [[ $(md5sum $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME | sed -e "s/ .*$//") == "$SPARK_MD5" ]]
-        then
+       
             # Unzip
             tar -xzf $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME -C $SPARK_INSTALL_LOCATION
             # Remove the compressed file
@@ -174,20 +137,6 @@ then
             # Install py4j
             pip install py4j
             SUCCESSFUL_SPARK_INSTALL=1
-        else
-            echo 'ERROR: Spark MD5 Hash does not match'
-            echo "$(md5sum $SPARK_INSTALL_LOCATION/$SPARK_FOLDER_NAME | sed -e "s/ .*$//") != $SPARK_MD5"
-            if [ $SPARK_INSTALL_TRY -lt 3 ]
-            then
-                echo -e '\nTrying Spark Install Again...\n'
-                SPARK_INSTALL_TRY=$[$SPARK_INSTALL_TRY+1]
-                echo $SPARK_INSTALL_TRY
-            else
-                echo -e '\nSPARK INSTALL FAILED\n'
-                echo -e 'Check the MD5 Hash and run again'
-                exit 1
-            fi
-        fi
     done
 else
     echo "Unable to detect Operating System"
